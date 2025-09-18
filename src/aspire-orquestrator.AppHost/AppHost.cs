3a00e1b8f3f74bc221fba847f4c4ae367510f0ee
renan-marcel <renanmarcelpaulinoleite@gmail.com>
2025-09-17 23:05:29 -0300

    Remove n8n-related configurations and dependencies
    This commit removes all configurations and dependencies related to the `n8n` service from the project. Key changes include:
    
    - Removed the `n8n` Dockerfile and associated licensing modifications.
    - Deleted `n8nService` and `apiService` configurations in `AppHost.cs`.
    - Removed the `aspire-orquestrator.ApiService` project from the solution.
    - Deleted references to the `Dockerfiles` folder in `aspire-orquestrator.AppHost.csproj`.
    - Cleaned up labels and user directives in the `Dockerfile`.
    
    These changes reflect a deprecation or refactoring of `n8n` components.

--- src/aspire-orquestrator.AppHost/AppHost.cs
+++ src/aspire-orquestrator.AppHost/AppHost.cs
@@ -3,14 +3,4 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 
-var file = new FileInfo(@"..\..\dockerfiles\n8n\Dockerfile");
-
-var apiService = builder.AddProject<Projects.aspire_orquestrator_ApiService>("apiservice")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health");
-
-var n8nService = builder.AddDockerfile("n8n", file.FullName)
-    .WithEndpoint(port: 5678, isExternal: true, targetPort: 5678)
-    .WithVolume("../../data/n8n", "/home/node/.n8n");
-
 await builder.Build().RunAsync();
